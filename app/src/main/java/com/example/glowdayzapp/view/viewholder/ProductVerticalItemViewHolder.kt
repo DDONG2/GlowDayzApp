b2e5dd320cd3f3a0ad6ee9510ddb77390bfe28bb
DESKTOP-OI19NCO\ppyum <doeon93@naver.com>
2021-07-15 18:05:27 +0900

    add : 글라이드 추가 및 리스트 뷰 작업

--- app/src/main/java/com/example/glowdayzapp/view/viewholder/ProductVerticalItemViewHolder.kt
+++ app/src/main/java/com/example/glowdayzapp/view/viewholder/ProductVerticalItemViewHolder.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.glowdayzapp.databinding.ItemVerticalProductBinding
 import com.example.glowdayzapp.model.vo.ProductVO
 
@@ -18,9 +19,36 @@ class ProductVerticalItemViewHolder (itemView: View, private val context: Contex
 
     fun bind(item: ProductVO) {
 
-        item.productTitle.let {
-            binding.productTitleTextView.text = it
+        item.imageUrl?.let{
+            Glide.with(context)
+                .load(it)
+                .into(binding.productImage)
         }
+
+        item.productRank?.let{
+            binding.productNumber.text = it
+        }
+
+        item.brand.brandTitle?.let {
+            binding.brandTitle.text = it
+        }
+
+        item.productTitle?.let {
+            binding.productTitle.text = it
+        }
+
+        item.ratingAvg?.let {
+            binding.ratingAvg.text = it.toString()
+        }
+
+        item.reviewCount?.let {
+            binding.reviewCount.text = "(리뷰 " + it + ")"
+        }
+
+
+
+
+
 
     }
 
